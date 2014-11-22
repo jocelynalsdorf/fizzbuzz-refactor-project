a6adc17f182d61acff7ca03dbc217e5e39ba8305
jocelynalsdorf <jfrodsla@gmail.com>
2014-11-22 09:16:56 +0700

    wrapped gamein functionand called function with onclick of button so that you canplay multiple times

--- main.js
+++ main.js
@@ -1,33 +1,44 @@
 $(document).ready(function(){
 
-var un = +prompt("Enter a number and only a number!");
     
+$('#play-btn').click(function(){
+     return fbGame();
+    });
+        
+   
+    var fbGame = function() {
+
+var un = +prompt("Enter a number and only a number!");   
 var fizz = "fizz!";
 var buzz = "buzz!";
 var fizzBuzz = "fizzbuzz!!";
               
-    if (un % 1 != 0) {
+ if (un % 1 != 0) {
         alert("no decimals and no letters allowed");
     }
-    else{
+    else {
     for (var i = 1; i <= un; i++) {
 	
 	if (i % 15 === 0) {
-		$("p").append("<span class=\"fb\">" + fizzBuzz + "          </span>");
+		$("p").append("<span class=\"fb\">" + fizzBuzz + "</span>");
 	}
+
 	else if (i % 3 === 0){
 		$("p").append("<span class=\"f\">" + fizz + "</span>");
 
 	}
+
 	else if (i % 5 === 0) {
 		$("p").append("<span class=\"b\">" + buzz + "</span>");
 	}
 
 	else {
 		$("p").append("<span>" + i + "</span>");
-	}
-}
-    }         
+	  }
+
+    }
+  } 
+ }
 });
 
 
